7b0fcf97ea4aea7f460a74f7d19e6eb52121dc9c
Paulo Campos Filho <pezaum@gmail.com>
2025-03-09 04:06:45 -0700

    Initial commit.

---
+++ Models/Classes/CSVProcessor.cs
@@ -0,0 +1,16 @@
+using PreprocessorApp.Models.Interfaces;
+
+namespace PreprocessorApp.Models.Classes;
+
+public class CSVProcessor : IProcessor
+{
+    public void Process()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Models/Interfaces/IProcessor.cs
@@ -0,0 +1,8 @@
+namespace PreprocessorApp.Models.Interfaces
+{
+    public interface IProcessor
+    {
+      public void Process();
+      public void Save();
+    }
+}
