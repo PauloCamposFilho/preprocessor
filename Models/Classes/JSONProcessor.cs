79bc909a5315785a0de054d8e0f9eae410d6a53a
Paulo Campos Filho <pezaum@gmail.com>
2025-03-09 06:15:19 -0700

    Add body to method Process

--- Models/Classes/JSONProcessor.cs
+++ Models/Classes/JSONProcessor.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
 using PreprocessorApp.Models.Interfaces;
 
 namespace PreprocessorApp.Models.Classes;
@@ -8,13 +10,39 @@ public class JSONProcessor() : IProcessor
   private StringBuilder _sb = new StringBuilder();
   private int processedRows = 0;
 
-    public void Process(string inputFile)
+  public void Process(string inputFile)
+  {
+    try
     {
-        throw new NotImplementedException();
-    }
+      string JSONContent = File.ReadAllText(inputFile);
+      var JSONArray = JArray.Parse(JSONContent);
 
-    public void Save(string outputFile)
-    {
-        throw new NotImplementedException();
+      if (JSONArray.Count == 0)
+      {
+        throw new Exception("JSON file is empty");
+      }
+
+      var headers = ((JObject)JSONArray[0]).Properties().Select(p => p.Name).ToArray();
+
+      foreach (var item in JSONArray)
+      {
+        var values = headers.Select(p => item[p]?.ToString() ?? string.Empty);
+        _sb.AppendLine(string.Join("|", values));
+        processedRows++;
+      }
     }
+    catch(JsonException ex)
+    {
+      Console.WriteLine($"JSON Error: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"Error: {ex.Message}");
+    }
+  }
+
+  public void Save(string outputFile)
+  {
+    throw new NotImplementedException();
+  }
 }
