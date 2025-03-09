b2d745546bd3ddaf6014bf90c2de026b7ae04748
Paulo Campos Filho <pezaum@gmail.com>
2025-03-09 05:18:21 -0700

    Add fields and method declarations

--- Models/Classes/ExcelProcessor.cs
+++ Models/Classes/ExcelProcessor.cs
@@ -1,6 +1,21 @@
+using System.Text;
+using PreprocessorApp.Models.Interfaces;
+
 namespace PreprocessorApp.Models.Classes;
 
-public class ExcelProcessor()
+public class ExcelProcessor(string inputFile, string outputFile = "output.txt") : IProcessor
 {
-  
+  private string _inputFile = inputFile;
+  private string _outputFile = outputFile;
+  private StringBuilder _sb = new StringBuilder();
+  private int processedRows = 0;
+
+  public void Process()
+  {
+
+  }
+  public void Save()
+  {
+    
+  }
 }
