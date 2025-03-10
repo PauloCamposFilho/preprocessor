ccf86aef8f44e7f69618df2314a12baa94a6781e
Paulo Campos Filho <pezaum@gmail.com>
2025-03-09 21:10:26 -0700

    Add initial CSV tests

---
+++ PreprocessorApp.Tests/CSVProcessorTests.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using System.IO;
+using PreprocessorApp.Models.Classes;
+namespace PreprocessorApp.Tests;
+
+public class CSVProcesorTests
+{
+    [Fact]
+    public void Process_ValidJSON_ReturnsFlatFileContent()
+    {
+        // Arrange
+        string inputPath = Path.Combine("tests", "input.csv");
+        string expectedOutputPath = Path.Combine("tests", "outputs", "outputCSV.txt");
+        string tempOutputPath = Path.GetTempFileName();
+        var processor = new CSVProcessor();
+
+        // Act
+        processor.Process(inputPath, tempOutputPath);
+
+        // Assert
+        string actualOutput = File.ReadAllText(tempOutputPath);
+        string expectedOutput = File.ReadAllText(expectedOutputPath);
+        Assert.Equal(expectedOutput, actualOutput);
+
+        // Cleanup
+        File.Delete(tempOutputPath);
+    }
+}
